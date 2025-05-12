cc524dc6989c4a3510adc38d582eede8cc2c400b
Yuragranovsky <yuragranovsky@gmail.com>
2025-05-12 16:03:15 +0300

    JS standard types in TS

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+let a: number;
+a = 10;
+// a = "abc"
+a = 20;
+let s = "abc";
+//s =10
+let b = true;
+b = !!a;
+b = false;
+const c = 10;
+//c =11;
+const arr = [ 1, 2, 3];
+//arr.push("abc");
+arr.length = 0;
+arr.push(3);
+const arr1 = [1, 2, 3, "abc"];
+const arr2: number[] = [];
+const arr3: number[][] = [];
+function fun(a:number[][]): void {
+  
+}
+type Person = {id: number, name: string};
+
+const person: Person = {id: 123, name: "Vasya"};
