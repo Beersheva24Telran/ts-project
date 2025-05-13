124d19bbba2b995fb413518bdd2c397919bca4b8
Yuragranovsky <yuragranovsky@gmail.com>
2025-05-13 12:59:33 +0300

    TS specific types

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,43 @@
-let a: number;
-a = 10;
-// a = "abc"
-a = 20;
-let s = "abc";
-//s =10
-let b = true;
-b = !!a;
-b = false;
-const c = 10;
-//c =11;
-const arr = [ 1, 2, 3];
-//arr.push("abc");
-arr.length = 0;
-arr.push(3);
-const arr1 = [1, 2, 3, "abc"];
-const arr2: number[] = [];
-const arr3: number[][] = [];
-function fun(a:number[][]): void {
-  
+type Color = "blue" | "red" | "green" | "yellow"
+//let color: Color = "kuku" 
+let color = "red"
+enum DayOfWeek {
+    Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sun=0, Mon, Tue, Wed, Thu, Fri, Sat
 }
-type Person = {id: number, name: string, age?: number, address?: {city:string, street: string}};
+console.log(DayOfWeek.Sunday === DayOfWeek.Sun);
+//any -  type that is not recommended for using
+let a: any = 10;
+//a.startWith();
+a = "abc"
+a++;
+//unknown - type like "any" but recommended
+let b: unknown = 10;
+typeof b === "number" && b++
+b = "abc"
+typeof b === "string" && b.startsWith("ab");
+//tuple
+const tupl1: [number, string] = [1, "2"];
+tupl1[0]++;
+tupl1.push("kuku"); //TS issue
+//index signature
+function getOccurrencesObject(array: string[]): {[key: string]:number} {
+    return array.reduce((acc: {[key: string]:number} , cur) => ({...acc, [cur]: acc[cur] ? ++acc[cur]: 1}), {})
+}
+const occurrences = getOccurrencesObject(["abc", "abc", "lmn", "abc", "a", "lmn"]);
+console.log(occurrences)
+type Person = {
+    id: number;
+    name: string;
+    age: number;
+    gender?: string;
+    address?: {
+        city: string;
+        street: string;
+    }
+}
+const person: Person = {id:123, name: "Vasya", age:25};
+let field: keyof Person;
+field = "address"
+person[field] //permitted square braces notation
 
-const person: Person = {id: 123, name: "Vasya"};
-person.age && person.age++;
-person.address?.street //expression has union type string | undefined
-person.address && person.address.city; //expression has type string
-person.address!.city; //expression has type string
+
